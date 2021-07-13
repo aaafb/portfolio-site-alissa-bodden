2c1cafa225a7a90cfcada604d7d5791d263deaf3
aaafb <85185608+aaafb@users.noreply.github.com>
2021-07-13 10:42:45 -0400

    Add "Read more" link on About page

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ function menuToggle() {
     }
     
 }
+
+function expandText() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMore");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+    }
+}
