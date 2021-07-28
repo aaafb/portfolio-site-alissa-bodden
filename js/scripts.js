25aaba50a621a663972d513c7ab0054a0cc486a5
aaafb <85185608+aaafb@users.noreply.github.com>
2021-07-28 15:27:47 -0400

    Add code for link to copy email address

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// RESPONSIVE NAV BAR AND HAMBURGER MENU
 function menuToggle() {
     var x = document.getElementById("myNavtoggle");
 
@@ -14,6 +14,7 @@ function menuToggle() {
     
 }
 
+// READ MORE/LESS LINKS ON ABOUT PAGE
 function toggleExpandText() {
 	var dots = document.getElementById('dots')
 	var moreText = document.getElementById('more')
@@ -34,3 +35,51 @@ function toggleExpandText() {
 	}
 }
 
+// COPY EMAIL ADDRESS
+// grab all elements with the copy-click class
+const links = document.querySelectorAll('.copy-click');
+// assign class names to object properties so they can be added to HTML elements dynamically, at least in part to apply CSS at specific states/times
+const cls = {
+  copied: 'is-copied',
+  hover: 'is-hovered' };
+
+// (i think) assigns shorter name "str" to longer initial name of const variable so it's easier to reference
+const copyToClipboard = str => {
+// create an input element in the HTML DOM (??) with the reference name "el"
+const el = document.createElement('input');
+  // ___ ? ___ : ___ is a shortform if/else statement...
+  str.dataset.copyString ? el.value = str.dataset.copyString : el.value = str.text;
+  // assign properties to the new input element
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.opacity = 0;
+  // add the new input element to the end of the HTML body
+  document.body.appendChild(el);
+  // select the contents of the input element
+  el.select();
+  // copy the selected contents of input element
+  document.execCommand('copy');
+  // remove the input element from the document
+  document.body.removeChild(el);
+};
+
+const clickInteraction = e => {
+  e.preventDefault();
+  copyToClipboard(e.target);
+  e.target.classList.add(cls.copied);
+  setTimeout(() => e.target.classList.remove(cls.copied), 1000);
+  setTimeout(() => e.target.classList.remove(cls.hover), 700);
+};
+
+Array.from(links).forEach(link => {
+  link.addEventListener('click', e => clickInteraction(e));
+  link.addEventListener('keypress', e => {
+    if (e.keyCode === 13) clickInteraction(e);
+  });
+  link.addEventListener('mouseover', e => e.target.classList.add(cls.hover));
+  link.addEventListener('mouseleave', e => {
+    if (!e.target.classList.contains(cls.copied)) {
+      e.target.classList.remove(cls.hover);
+    }
+  });
+});
